193c6ece69017766a72bc0928084ea2e78305511
logan britt <logan.r.britt@protonmail.com>
2024-10-19 00:16:15 -0400

    reworking how deploy works

--- Deploy/help.py
+++
@@ -1,8 +0,0 @@
-MESSAGE = [
-    'The acceptable command line arguments and there values follow:',
-    '\targument: --help -H values: NONE',
-]
-
-def print_help() -> None:
-    for line in MESSAGE:
-        print(line)
